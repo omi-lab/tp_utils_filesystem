95707be9b54a83079e79d8fe80b53fcba630c3d3
Tom Paynter <tompaynter@tdpe.co.uk>
2025-08-25 11:58:46 +0100

    Merge from studio engine.

--- dependencies/cmake.cmake
+++ dependencies/cmake.cmake
@@ -3,9 +3,8 @@ if(WIN32)
   # list(APPEND TP_LIBRARIES "libstdc++fs")
   # list(APPEND TP_LIBRARIES "c++fs")
   # list(APPEND TP_LIBRARIES "libc++fs")
-  list(APPEND TP_DEFINES "TP_FILESYSTEM")
-else()
+elseif(NOT APPLE)
   list(APPEND TP_LIBRARIES "-lstdc++fs")
-  list(APPEND TP_DEFINES "TP_FILESYSTEM")
 endif()
-
+  
+list(APPEND TP_DEFINES "TP_FILESYSTEM")
